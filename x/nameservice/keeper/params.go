8741992fccf3267904741735ddce0f19f8e5d98a
Giancarlos Salas <giansalex@gmail.com>
2020-09-22 08:58:54 -0500

    Add params keeper

--- x/nameservice/keeper/params.go
+++ x/nameservice/keeper/params.go
@@ -1,22 +1,31 @@
 package keeper
-/*
-// TODO: Define if your module needs Parameters, if not this can be deleted
 
 import (
-	"time"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/giansalex/nameservice/x/nameservice/types"
 )
 
+// MinPrice - Min price for name
+func (k Keeper) MinPrice(ctx sdk.Context) (res uint64) {
+	k.paramspace.Get(ctx, types.KeyMinPrice, &res)
+	return
+}
+
+// BondDenom - Bondable coin denomination
+func (k Keeper) BondDenom(ctx sdk.Context) (res string) {
+	k.paramspace.Get(ctx, types.KeyBondDenom, &res)
+	return
+}
+
 // GetParams returns the total set of nameservice parameters.
 func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
-	k.paramspace.GetParamSet(ctx, &params)
-	return params
+	return types.NewParams(
+		k.MinPrice(ctx),
+		k.BondDenom(ctx),
+	)
 }
 
 // SetParams sets the nameservice parameters to the param space.
 func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
 	k.paramspace.SetParamSet(ctx, &params)
 }
-*/
