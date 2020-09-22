492ab8acf06f749a36946a61a1771c0c9745c4d6
Giancarlos Salas <giansalex@gmail.com>
2020-09-22 08:52:03 -0500

    Add params

--- x/nameservice/types/params.go
+++ x/nameservice/types/params.go
@@ -1,57 +1,131 @@
 package types
 
 import (
+	"errors"
 	"fmt"
+	"strings"
 
-	"github.com/cosmos/cosmos-sdk/x/params"
+	yaml "gopkg.in/yaml.v2"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	paramtypes "github.com/cosmos/cosmos-sdk/x/params"
 )
 
 // Default parameter namespace
 const (
-	DefaultParamspace = ModuleName
-	// TODO: Define your default parameters
+	DefaultParamspace        = ModuleName
+	DefaultMinPrice   uint64 = 100000
 )
 
-// Parameter store keys
 var (
-	// TODO: Define your keys for the parameter store
-	// KeyParamName          = []byte("ParamName")
+	// KeyMinPrice Min Price Key
+	KeyMinPrice = []byte("MinPrice")
+	// KeyBondDenom Coin Denom key
+	KeyBondDenom = []byte("BondDenom")
 )
 
 // ParamKeyTable for nameservice module
-func ParamKeyTable() params.KeyTable {
-	return params.NewKeyTable().RegisterParamSet(&Params{})
+func ParamKeyTable() paramtypes.KeyTable {
+	return paramtypes.NewKeyTable().RegisterParamSet(&Params{})
 }
 
 // Params - used for initializing default parameter for nameservice at genesis
 type Params struct {
-	// TODO: Add your Paramaters to the Paramter struct
-	// KeyParamName string `json:"key_param_name"`
+	MinPrice  uint64 `json:"min_price"`
+	BondDenom string `json:"bond_denom"`
 }
 
 // NewParams creates a new Params object
-func NewParams(/* TODO: Pass in the paramters*/) Params {
+func NewParams(minPrice uint64, bondDenom string) Params {
 	return Params{
-		// TODO: Create your Params Type
+		MinPrice:  minPrice,
+		BondDenom: bondDenom,
 	}
 }
 
 // String implements the stringer interface for Params
 func (p Params) String() string {
-	return fmt.Sprintf(`
-	// TODO: Return all the params as a string
-	`, )
+	out, _ := yaml.Marshal(p)
+	return string(out)
 }
 
 // ParamSetPairs - Implements params.ParamSet
-func (p *Params) ParamSetPairs() params.ParamSetPairs {
-	return params.ParamSetPairs{
+func (p *Params) ParamSetPairs() paramtypes.ParamSetPairs {
+	return paramtypes.ParamSetPairs{
 		// TODO: Pair your key with the param
-		// params.NewParamSetPair(KeyParamName, &p.ParamName),
+		paramtypes.NewParamSetPair(KeyMinPrice, &p.MinPrice, validateMinPrice),
+		paramtypes.NewParamSetPair(KeyBondDenom, &p.BondDenom, validateBondDenom),
 	}
 }
 
 // DefaultParams defines the parameters for this module
 func DefaultParams() Params {
-	return NewParams( /* TODO: Pass in your default Params */ )
+	return NewParams(
+		DefaultMinPrice,
+		sdk.DefaultBondDenom,
+	)
+}
+
+// unmarshal the current staking params value from store key or panic
+func MustUnmarshalParams(cdc *codec.Codec, value []byte) Params {
+	params, err := UnmarshalParams(cdc, value)
+	if err != nil {
+		panic(err)
+	}
+
+	return params
+}
+
+// unmarshal the current staking params value from store key
+func UnmarshalParams(cdc *codec.Codec, value []byte) (params Params, err error) {
+	err = cdc.UnmarshalBinaryBare(value, &params)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+// validate a set of params
+func (p Params) Validate() error {
+	if err := validateMinPrice(p.MinPrice); err != nil {
+		return err
+	}
+
+	if err := validateBondDenom(p.BondDenom); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func validateMinPrice(i interface{}) error {
+	v, ok := i.(uint64)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", i)
+	}
+
+	if v <= 0 {
+		return fmt.Errorf("min price must be positive: %d", v)
+	}
+
+	return nil
+}
+
+func validateBondDenom(i interface{}) error {
+	v, ok := i.(string)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", i)
+	}
+
+	if strings.TrimSpace(v) == "" {
+		return errors.New("bond denom cannot be blank")
+	}
+
+	if err := sdk.ValidateDenom(v); err != nil {
+		return err
+	}
+
+	return nil
 }
